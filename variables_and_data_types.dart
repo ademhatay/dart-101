eca97abea9bcb7be3d62f0f4c524c92e82474967
Adem Hatay <hatayadem5@gmail.com>
2023-10-04 17:35:37 +0300

    variables & types

---
+++ variables_and_data_types.dart
@@ -0,0 +1,23 @@
+void main(List<String> args) {
+  // Variables
+  // String name = 'John';
+  // int age = 25;
+  // double height = 1.75;
+  // bool isMarried = false;
+
+  String name = 'John';
+  int age = 25;
+  double height = 1.75;
+  bool isMarried = false;
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: $height');
+  print('Married: $isMarried');
+
+  var name2 = 'John';
+  var age2 = 25;
+
+  print('Name2: $name2');
+  print('Age2: $age2');
+}
