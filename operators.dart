997392b9b73dd8c6cf0a41743b43bfd36b1b6025
Adem Hatay <hatayadem5@gmail.com>
2023-10-04 18:03:15 +0300

    operators.

---
+++ operators.dart
@@ -0,0 +1,31 @@
+void main(List<String> args) {
+  // Arithmetic Operators (+, -, *, /, %, ++, --)
+  int a = 10;
+  int b = 20;
+  print(a + b);
+  print(a - b);
+  print(a * b);
+  print(a / b);
+  print(a % b);
+  print(a++);
+  print(a--);
+
+  print("\n");
+
+  // Relational Operators (==, !=, >, <, >=, <=)
+  print(a == b);
+  print(a != b);
+  print(a > b);
+  print(a < b);
+  print(a >= b);
+  print(a <= b);
+
+  print("\n");
+
+  // Logical Operators (&&, ||, !)
+  bool x = true;
+  bool y = false;
+  print(x && y);
+  print(x || y);
+  print(!x);
+}
